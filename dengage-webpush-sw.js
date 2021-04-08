8f85e85c0cfc8941909692d424b84015ce937978
Sergen Tufanoğulları <sergen@whitehorse.technology>
2021-04-08 12:55:24 +0300

    fix: sdk change

--- dengage-webpush-sw.js
+++ dengage-webpush-sw.js
@@ -3,6 +3,6 @@ var swUrl = new URL(location);
 var accountId = swUrl.searchParams.get('account_id') || '';
 var appGuid = swUrl.searchParams.get('app_guid') || '';
 if (accountId && appGuid) {
-    importScripts("https://ru-pub.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
+    importScripts("https://rupub.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
 }
         
